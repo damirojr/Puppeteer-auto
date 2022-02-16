dca959ed5f2fc4d4746cab6c91f87b5954abc884
Damiro M <97421651+damirojr@users.noreply.github.com>
2022-02-16 03:10:33 -0300

    Update main.js

--- main.js
+++ main.js
@@ -3,8 +3,7 @@ const puppeteer = require('puppeteer');
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
- // const var1 = 'dolar';
-  //const var2 = 'real';
+  
   const paginaAdress = `https://github.com/damirojr/Puppeteer-auto`
 
   await page.goto(paginaAdress);
