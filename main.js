9ef545effc72012162128f78066a9c6e26b92a0c
Damiro M <97421651+damirojr@users.noreply.github.com>
2022-02-22 20:43:33 -0300

    Update main.js

--- main.js
+++ main.js
@@ -2,10 +2,8 @@ const puppeteer = require('puppeteer');
 
 (async () => {
   const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  
+  const page = await browser.newPage()
   const paginaAdress = `https://github.com/damirojr/Puppeteer-auto`
-
   await page.goto(paginaAdress);
   await page.screenshot({ path: 'teste.jpg'});
   
